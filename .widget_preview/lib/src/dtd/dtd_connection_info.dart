20e80485afac71a46e6873f68a18b893b60dd073
Yuan En <siayuanen@gmail.com>
2026-09-21 20:38:00 +0800

    all
    all

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:60981/SBO0q4YkNIU=';
+const String kWidgetPreviewService =
+    'widget-preview-b526d014-a2f2-40a0-80e6-6dd05b7f5169';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-b526d014-a2f2-40a0-80e6-6dd05b7f5169';
+const String kProjectRootPath =
+    r'/Users/yuanen/Desktop/Projects/2025/cpmad-siayuanen-project-dotted';
